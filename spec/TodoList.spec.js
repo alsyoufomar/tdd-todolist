c8e9f15ef72205f16d57a2217fa42f65108ff206
omar <omaralsoyouf@gmail.com>
2022-01-12 09:11:01 +0000

    Omar Submission

--- spec/TodoList.spec.js
+++ spec/TodoList.spec.js
@@ -1,5 +1,31 @@
 const TodoList = require("../src/TodoList.js")
 
+const one = {
+  id: 1,
+  text: "turn the heating on!",
+  status: "incomplete"
+}
+const two = {
+  id: 2,
+  text: "make the bed!",
+  status: "incomplete"
+}
+const three = {
+  id: 3,
+  text: "Make some food!",
+  status: "complete"
+}
+const four = {
+  id: 4,
+  text: "Wash the dishes!",
+  status: "incomplete"
+}
+const five = {
+  id: 5,
+  text: "Feed the cat!",
+  status: "incomplete"
+}
+
 describe("TodoList", () => {
   let todoList
 
@@ -9,14 +35,114 @@ describe("TodoList", () => {
 
   it("creates a todo item", () => {
     // set up
-    const expected = {
-      id: 1,
-      text: "turn the heating on!",
-      status: "incomplete"
-    }
+    expected = one
     // execute
-    const result = todoList.create("turn the heating on!")
+    result = todoList.create("turn the heating on!")
     // verify
     expect(result).toEqual(expected)
   })
-})
+
+  it("gets all todos", () => {
+    // set up
+    const expected = [one, two]
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("make the bed!")
+    const result = todoList.allTodo
+    // verify
+    expect(result).toEqual(expected)
+  })
+
+  it("set todo: complete by ID", () => {
+    // set up
+    expected = three
+    // execute
+    todoList.create("Do nothing!")
+    todoList.create("turn the heating on!")
+    const TODO = todoList.create("Make some food!")
+    todoList.setComplete(3)
+    result = TODO
+    // verify
+    expect(result).toEqual(expected)
+  })
+
+  it("return todos by status", () => {
+    // set up
+    expected = [one, two, five]
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("make the bed!")
+    todoList.create("Make some food!")
+    todoList.create("Wash the dishes!")
+    todoList.create("Feed the cat!")
+    todoList.setComplete(3)
+    todoList.setComplete(4)
+    result = todoList.getTodosByStatus("incomplete")
+    // verify
+    expect(result).toEqual(expected)
+  })
+
+  it("return all complete todo's", () => {
+    // // set up
+    expected = [three]
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("make the bed!")
+    todoList.create("Make some food!")
+    todoList.create("Wash the dishes!")
+    todoList.create("Feed the cat!")
+    todoList.setComplete(3)
+    result = todoList.getTodosByStatus("complete")
+    // verify
+    expect(result).toEqual(expected)
+  })
+
+  it("search for TODO by id", () => {
+    // set up
+    expected = one
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("Make some food!")
+    todoList.create("Wash the dishes!")
+    todoList.create("Feed the cat!")
+    result = todoList.getTodo(1)
+
+    // verify
+    expect(result).toEqual(expected)
+  })
+
+  it("returns message if Todo not found", () => {
+    // set up
+    expected = "Todo not found"
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("Make some food!")
+    todoList.create("Wash the dishes!")
+    todoList.create("Feed the cat!")
+    result = todoList.getTodo(5)
+    // verify
+    expect(result).toEqual(expected)
+  })
+
+  it("deletes a todo item", () => {
+    // set up
+    expected = [one]
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("Make some food!")
+    result = todoList.deleteTodo(2)
+    // verify
+    expect(result).toEqual(expected)
+  })
+
+  it("returns error message for invalid input", () => {
+    // set up
+    expected = "invalid ID"
+    // execute
+    todoList.create("turn the heating on!")
+    todoList.create("Make some food!")
+    result = todoList.deleteTodo("null")
+    // verify
+    expect(result).toEqual(expected)
+  })
+}) 
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,5 +1,48 @@
 class TodoList {
+    constructor() {
+        this.list = []
+        this.nextID = 1
+    }
+    create(str) {
+        const todo = {
+            id: this.nextID,
+            text: str,
+            status: "incomplete"
+        }
+        this.nextID++
+        this.list.push(todo)
+        return todo
+    }
 
+    get allTodo() {
+        return this.list
+    }
+    k
+    setComplete(id) {
+        this.list[id - 1]["status"] = "complete"
+    }
+
+    getTodosByStatus(status) {
+        return this.list.filter(todo => todo["status"] === status)
+
+    }
+
+    getTodo(id) {
+        for (let todo of this.list) {
+            if (todo['id'] === id)
+                return todo
+        }
+        return "Todo not found"
+    }
+
+    deleteTodo(id) {
+        if (typeof (id) !== "number") return "invalid ID"
+        return this.list.filter(todo => todo['id'] !== id)
+    }
+
+    getTodosByDate(time) {
+        return this.list.filter(todo => todo['date'] == time)
+    }
 }
 
 module.exports = TodoList
